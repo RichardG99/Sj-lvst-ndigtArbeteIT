0a3889de73906e7a3bacae5ddf6a6eb4354de8f3
Nicke535 <nicke@lofwenberg.com>
2021-04-22 15:06:04 +0200

    Hopefully pointed Parse to the correct address

--- parse-server/src/shared/common.js
+++ parse-server/src/shared/common.js
@@ -1,6 +1,6 @@
 const Parse = require('parse');
 
 Parse.initialize('myAppId', 'DreamScape');
-Parse.CoreManager.set('SERVER_URL', 'http://localhost:1337/parse');
+Parse.CoreManager.set('SERVER_URL', 'http://dream.cellis.studio:1337/parse');
 
 export default Parse;
