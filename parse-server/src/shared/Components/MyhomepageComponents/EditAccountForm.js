7c75a225393298defeaa10696045c7db0f6c9536
Nicke535 <nicke@lofwenberg.com>
2021-04-27 11:42:00 +0200

    Actually imported parse in the edit form

--- parse-server/src/shared/Components/MyhomepageComponents/EditAccountForm.js
+++ parse-server/src/shared/Components/MyhomepageComponents/EditAccountForm.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styles from '../../styles';
+import Parse from '../../common';
 
 const formStyle = {
   boxSizing: 'border-box',
