e4759de4cb3b7b3ca1b528f495e2d6d84609ffb2
Nicke535 <nicke@lofwenberg.com>
2021-05-07 13:51:43 +0200

    Improved change password style and functionality

--- parse-server/src/shared/Components/MyhomepageComponents/EditPasswordForm.js
+++ parse-server/src/shared/Components/MyhomepageComponents/EditPasswordForm.js
@@ -18,7 +18,9 @@ class EditPasswordForm extends React.Component {
     super(props);
     const properties = this.props; // TODO this might create issues...?
     this.state = {
-      password: properties.password,
+      oldPassword: "",
+      newPassword: "",
+      confirmPassword: "",
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -35,22 +37,31 @@ class EditPasswordForm extends React.Component {
 
     event.preventDefault(); // Prevent opening a new page when clicked
 
-    if (tmpState.password === '') {
-      alert("Your password cannot consist of 0 characters");
+    if (tmpState.newPassword == '') {
+      alert("Your password cannot consist of 0 characters!");
+    } if (tmpState.newPassword !== tmpState.confirmPassword) {
+      alert("Confirmation password must match new password");
     } else {
-      //Gets our user and sets their various attributes
+      //Gets our user
       const user = Parse.User.current();
-      user.set('password', tmpState.password);
 
-      user.save().then(() => {
-        // Refresh our page to ensure fresh data, and give a feed-forward feeling for the user
-        console.log(`Successfully changed user password`);
-        window.location.reload();
-      }).catch((error) => {
-        // Alert the user in case something went wrong with data saving
-        alert("Something went wrong when changing your password! Try again later (your password has not been changed)"); 
+      //Tries to verify the user's password, and changes it if succesful
+      Parse.User.verifyPassword(user.getUsername(), tmpState.oldPassword).then(()=> {
+        user.set('password', tmpState.password);
+        user.save().then(() => {
+          // Refresh our page to ensure fresh data, and give a feed-forward feeling for the user
+          console.log(`Successfully changed user password`);
+          window.location.reload();
+        }).catch((error) => {
+          // Alert the user in case something went wrong with data saving
+          alert("Something went wrong when changing your password! Try again later (your password has not been changed)"); 
+          console.log(`Error ${error.code} ${error.message}`);
+        });
+      }, (error) => {
+        //Incorrect old user password, or some other error. Log it, and inform the user that most likely they've not written the correct password
+        alert("Could not verify user to server; either your old password is incorrect, or a server-side issue has occurred."); 
         console.log(`Error ${error.code} ${error.message}`);
-      });
+      })
     }
   }
 
@@ -61,14 +72,40 @@ class EditPasswordForm extends React.Component {
       <form>
         <div style={formStyle}>
           <div style={inputDivStyle}>
-            <label htmlFor="password">
-              Password:
+            <label htmlFor="oldPassword">
+              Old Password:
               <input
-                type="text"
-                value={tmpState.password}
+                type="password"
+                value={tmpState.oldPassword}
+                onChange={this.handleChange}
+                placeholder="Enter existing password"
+                name="oldPassword"
+                required
+              />
+            </label>
+          </div>
+          <div style={inputDivStyle}>
+            <label htmlFor="newPassword">
+              New Password:
+              <input
+                type="password"
+                value={tmpState.newPassword}
                 onChange={this.handleChange}
                 placeholder="Enter new password"
-                name="password"
+                name="newPassword"
+                required
+              />
+            </label>
+          </div>
+          <div style={inputDivStyle}>
+            <label htmlFor="confirmPassword">
+              Confirm New Password:
+              <input
+                type="password"
+                value={tmpState.confirmPassword}
+                onChange={this.handleChange}
+                placeholder="Enter new password again"
+                name="confirmPassword"
                 required
               />
             </label>
@@ -81,7 +118,6 @@ class EditPasswordForm extends React.Component {
   }
 }
 EditPasswordForm.propTypes = {
-  password: PropTypes.string.isRequired,
   handleCancel: PropTypes.func.isRequired,
 };
 
