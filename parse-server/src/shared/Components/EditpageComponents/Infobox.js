6866b2888f162d46d575b87d0504fe044a6087ee
Nicke535 <nicke@lofwenberg.com>
2021-05-11 15:40:29 +0200

    Testing

--- parse-server/src/shared/Components/EditpageComponents/Infobox.js
+++ parse-server/src/shared/Components/EditpageComponents/Infobox.js
@@ -54,7 +54,7 @@ class Infobox extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      extendedInfo: false,
+      extendedInfo: true,
     };
     this.toggleInfo = this.toggleInfo.bind(this);
   }
