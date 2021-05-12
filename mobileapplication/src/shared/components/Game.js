67c9e8496b785817beafbe133be58d152bf00607
Alexander Sellström <rednaxela5950@users.noreply.github.com>
2021-05-12 16:34:16 +0200

    forgot to send params

--- mobileapplication/src/shared/components/Game.js
+++ mobileapplication/src/shared/components/Game.js
@@ -66,7 +66,7 @@ export default class Game extends React.Component {
   speechToTextAPI = async (audio) => {
     const recordingURI = audio.getURI();
     const file_to_send = await FileSystem.readAsStringAsync(recordingURI, {encoding: FileSystem.EncodingType.Base64});
-    //const params = {audio_base64: file_to_send, OS: Platform.OS}; // Send audio + platform info
+    const params = {audio_base64: file_to_send, OS: Platform.OS}; // Send audio + platform info
     const converted_text = await Parse.Cloud.run("speechToTextCall", params);
     console.log(converted_text.finishedTranscript);
     return converted_text.finishedTranscript;
