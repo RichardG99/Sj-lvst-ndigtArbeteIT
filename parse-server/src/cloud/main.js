3cd8e5591c543449bfe52b2d012e58bfc8eff14b
Alexander Sellström <rednaxela5950@users.noreply.github.com>
2021-05-17 11:43:59 +0200

    delete voice recordings

--- parse-server/src/cloud/main.js
+++ parse-server/src/cloud/main.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const { IamAuthenticator } = require('ibm-watson/auth');
 const SpeechToTextV1 = require('ibm-watson/speech-to-text/v1');
+const Str = require('@supercharge/strings');
 
 const speechToText = new SpeechToTextV1({
   authenticator: new IamAuthenticator({
@@ -9,36 +10,27 @@ const speechToText = new SpeechToTextV1({
   serviceUrl: 'https://api.eu-de.speech-to-text.watson.cloud.ibm.com/instances/b21c4259-c531-453f-aed5-044be6ef3995',
 });
 
-/* In the client you need to create this object and send it as params
-const params = {
-  objectMode: true,
-  audio: "filename",
-  contentType: 'audio/flac',
-  model: 'en-US_BroadbandModel',
-  keywords: ['colorado', 'tornado', 'tornadoes'],
-  keywordsThreshold: 0.5,
-  maxAlternatives: 3,
-};*/
-
 Parse.Cloud.define('speechToTextCall', async (req, res) => {
   let base64data = req.params.audio;
-  fs.writeFileSync('file.webm', Buffer.from(base64data.replace('data:audio/webm; codecs=opus;base64,', ''), 'base64'));
+  const filename = 'voice/' + Str.random() + '.webm'
+  fs.writeFileSync(filename, Buffer.from(base64data.replace('data:audio/webm; codecs=opus;base64,', ''), 'base64'));
   const recognizeParams = {
-    audio: fs.createReadStream('file.webm'),
+    audio: fs.createReadStream(filename),
     contentType: 'audio/webm',
     wordAlternativesThreshold: 0.9,
     model: 'en-US_BroadbandModel',
     keywords: req.params.keywords,
     keywordsThreshold: 0.5,
+    endOfPhraseSilenceTime: 6.0,
   };
   
 return speechToText.recognize(recognizeParams)
   .then(speechRecognitionResults => {
-    //return "halloj";
-    //JSON.stringify(speechRecognitionResults, null, 2));
+    fs.unlinkSync(filename)
     return JSON.stringify(speechRecognitionResults, null, 2);
   })
   .catch(err => {
+    fs.unlinkSync(filename)
     console.log('error:', err);
     return "err";
   });
